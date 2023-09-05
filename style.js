2fb6fff3b1416c641af6cd184801947a677e3b0a
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-09-05 14:23:16 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,6 +1,7 @@
 function loadCSS(url, callback) {
   var link = document.createElement("link");
   link.href = url;
+  rel="stylesheet";
 
   link.onload = function () {
     // The CSS file has finished loading, so we can call the callback function
@@ -22,7 +23,7 @@ loadCSS("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.cs
       loadCSS("assets/vendor/swiper/swiper-bundle.min.css", function () {
         loadCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css", function () {
           loadCSS("assets/vendor/bootstrap/css/bootstrap.min.css", function () {
-            loadCSS("assets/css/style.css", rel="stylesheet" function () {
+            loadCSS("assets/css/style.css",  function () {
               // All CSS files have finished loading
               // You can now call the function or perform actions
               // that depend on the CSS being loaded.
