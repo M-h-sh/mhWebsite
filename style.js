5b9ce955c8a073dcc405b871290d94df2a7b1295
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-09-05 13:44:39 +0200

    Update style.js

--- style.js
+++ style.js
@@ -6,14 +6,11 @@ function loadCSS(url) {
 }
 
 // Load the CSS files
-loadCSS("assets/img/favicon.png"
-    rel = "icon");
+loadCSS("assets/img/favicon.png");
 loadCSS("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
 loadCSS("assets/vendor/bootstrap-icons/bootstrap-icons.css");
 loadCSS("assets/vendor/glightbox/css/glightbox.min.css");
 loadCSS("assets/vendor/swiper/swiper-bundle.min.css");
 loadCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
-loadCSS("assets/vendor/bootstrap/css/bootstrap.min.css"
-    rel = "stylesheet");
-loadCSS("assets/css/style.css"
-    rel = "stylesheet");
+loadCSS("assets/vendor/bootstrap/css/bootstrap.min.css");
+loadCSS("assets/css/style.css");
